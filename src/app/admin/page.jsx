c987f1f3f64a118a78691828a517ffc9505a25b4
Alvin Dimas Praditya <alvin.dimas.praditya@gmail.com>
2023-11-02 22:36:50 +0700

    feat: carousel create delete

--- src/app/admin/page.jsx
+++ src/app/admin/page.jsx
@@ -1,11 +1,13 @@
-"use client";
+'use client';
 import Header from "@/components/Header";
+import CarouselNews from "@/components/news/CarouselNews";
 import News from "@/components/news/News";
 
-export default function Admin(){
+export default function Home(){
     return (
         <main>
             <Header/>
+            <CarouselNews/>
             <News isAdmin={true}/>
         </main>
     );
--- src/app/page.jsx
+++ src/app/page.jsx
@@ -1,37 +1,13 @@
 'use client';
-import { Carousel } from "@mantine/carousel";
-import { Image } from "@mantine/core";
-import Autoplay from 'embla-carousel-autoplay';
 import Header from "../components/Header";
-import { useRef } from "react";
 import News from "../components/news/News";
-
-function Slide(){
-    return (
-        <Carousel.Slide>
-            <Image className="animate__animated animate__fadeIn" src={"https://picsum.photos/1080/720"}/>
-        </Carousel.Slide>
-    );
-}
-
+import CarouselNews from "@/components/news/CarouselNews";
 
 export default function Home(){
-    const autoplay = useRef(Autoplay({ delay: 4000 }));
     return (
         <main>
             <Header/>
-            <Carousel withIndicators withControls loop
-                slideSize={{ base: '100%', sm: '50%'}}
-                plugins={[autoplay.current]}
-                onMouseEnter={autoplay.current.stop}
-                onMouseLeave={autoplay.current.reset}
-                className="animate__animated animate__fadeIn">
-                <Slide/>
-                <Slide/>
-                <Slide/>
-                <Slide/>
-                <Slide/>
-            </Carousel>
+            <CarouselNews/>
             <News/>
         </main>
     );
